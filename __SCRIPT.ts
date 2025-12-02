a337416c0908909335120609a2a904b1e41809ee
nikgodda <96114399+nikgodda@users.noreply.github.com>
2025-12-02 15:43:13 +0500

    modified:   __SCRIPT.ts 	modified:   __STRINGS.json 	modified:   package-lock.json 	modified:   package.json 	modified:   src/GameModes/Example/ExampleGameMode.ts

--- __SCRIPT.ts
+++ __SCRIPT.ts
@@ -31,6 +31,24 @@ export class ExampleGameMode extends AGameMode {
 
         // Example usage of modlib (optional hint)
         modlib.ParseUI()
+
+        // Example usages of strings
+
+        mod.Message(`test`)
+
+        mod.Message(`static.message`)
+        mod.Message(`static.messageWithParams`, 1)
+
+        mod.stringkeys.static.stringkey
+
+        // @stringkeys dynamic.range: 1..2
+        for (let i = 0; i < 4; i++) {
+            mod.Message(`dynamic.range.${i}`)
+        }
+
+        // @stringkeys dynamic.list: Idle, Roam, Fight
+        const state = 'Idle'
+        mod.Message(`dynamic.list.${state}`)
     }
 }
 
