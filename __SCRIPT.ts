f41a7dc40101a9c1cd8513a7d393cd30adf04dc2
nikgodda <96114399+nikgodda@users.noreply.github.com>
2025-12-02 05:57:01 +0500

    modified:   __SCRIPT.ts 	modified:   __STRINGS.json 	modified:   package-lock.json 	modified:   package.json

--- __SCRIPT.ts
+++ __SCRIPT.ts
@@ -29,8 +29,6 @@ export class ExampleGameMode extends AGameMode {
     onGameModeStarted(): void {
         console.log('Cool Example GameMod!')
 
-        mod.DisplayHighlightedWorldLogMessage(mod.Message('test7'))
-
         // Example usage of modlib (optional hint)
         modlib.ParseUI()
     }
